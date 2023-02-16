6d79d2ddca102bc20100f8310595c6aee4c89793
FelipeBacab <124842799+FelipeBacab@users.noreply.github.com>
2023-02-15 22:40:28 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
-
+//importamos la app
 import App from "./App";
-
+//creamos un get.ELementById
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
